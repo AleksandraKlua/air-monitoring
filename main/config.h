450d561240eb7095c1c36c5e2ff1970b9b1da778
alexk <alex.klua@gmail.com>
2022-03-21 02:50:01 +0300

    fix: some problems with httpClient & dht22 data

--- main/config.h
+++ main/config.h
@@ -1,25 +1,27 @@
+#define DEVICE "ESP8266"
+#define TZ_INFO "MST-3MDT,M3.5.0/2,M10.5.0/3" // Set timezone string according to https://www.gnu.org/software/libc/manual/html_node/TZ-Variable.html, St. Petersburg
 // DHT sensor
-#define DHT_PIN 8
+#define DHT_PIN 2
 #define DHT_TYPE DHT22
 // LED
 #define LED_PIN 1
 #define LED_CLK 2
 #define LED_CS 3
 // WIFI
-#define WIFI_STA "STA"
-#define WIFI_SSID "wifi_name"
-#define WIFI_PASSWORD "password"
+
+#define WIFI_SSID "BD-2"
+#define WIFI_PASSWORD "Sinabon181"
 #define WL_CONNECTED 200
-// Influx
-#define INFLUX_HOST "host_name"
-#define INFLUX_ORG_ID "org_id"
-#define INFLUX_BUCKET "bucket"
-#define INFLUX_TOKEN "token"
+// InfluxDB
+#define INFLUXDB_URL "https://europe-west1-1.gcp.cloud2.influxdata.com"
+#define INFLUXDB_ORG "xelanotron@gmail.com"
+#define INFLUXDB_BUCKET "arduino"
+#define INFLUXDB_TOKEN "i46UR1ETkrURoN1mrZm9p1szsgwgik5ExajxBkhsNJUs8rv6imWMxzLgxcJbRPxd1nWna8DkHOKxVZRPhaILpA=="
 // Loki
-#define LOKI_USER "user_name"
-#define LOKI_API_KEY "api_key"
+#define LOKI_USER "xelanotron"
+#define LOKI_API_KEY "eyJrIjoiMTMyZGEwZDY3MzJiYjk0NDM3YWZjYzZhOTBmNGZjODBkZmY1Zjc2YyIsIm4iOiJhcmR1aW5vLWxvZ3MiLCJpZCI6NjA4NjYwfQ"
 // Metrics
 #define INTERVAL 30
 // Graphite
-#define GRAPHITE_USER "user_graphite"
+#define GRAPHITE_USER "xelanotron"
 #define GRAPHITE_API_KEY "api_key_graphite"
